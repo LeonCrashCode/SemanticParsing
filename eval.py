28b117bfafb6489449031b02a01e774df3eb5326
Jiangming <jmliunlp@gmail.com>
2017-12-13 16:31:46 +0000

    ^_^

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
+import sys
 import re
 class Eval:
-	def __init__(self, tags_info):
+	def __init__(self):
 		self.ignore = ["SDRS(", "DRS(", "NOT(", "NEC(", "POS(", "OR(", "DUPLEX(", "IMP(", ")"]
 		
 		self.kp = re.compile("^K[0-9]+\(?$")
@@ -12,8 +13,21 @@ class Eval:
 	def eval(self, outputs, golds):
 		assert len(outputs) == len(golds)
 
+		acc = 0.
+		p_base = 0.
+		r_base = 0.
 		for i in range(len(outputs)):
-			self._eval(outputs[i], golds[i])
+			a, b, c = self._eval(outputs[i], golds[i])
+			acc += a
+			p_base += b
+			r_base += c
+		P = acc / p_base
+		R = acc/ r_base
+		
+		F = 0
+		if acc != 0:
+			F = 2*P*R/ (P+R)
+		return P,R,F
 
 	def _eval(self, output, gold):
 		o = self._get_relation_set(output)
@@ -27,13 +41,7 @@ class Eval:
 			if item in g:
 				acc += 1
 
-		P = acc/p_base
-		R = acc/r_base
-
-		F = 0
-		if acc != 0:
-			F = 2*P*R/(P+R)
-		return P,R,F
+		return acc, p_base, r_base
 
 	def _get_relation_set(self, drs):
 		drs = drs.split()
@@ -52,6 +60,17 @@ class Eval:
 			elif self.sp.match(item):
 				pass
 			else:
+
 				re.append(item)
+		return re
+L1 = []
+for line in open(sys.argv[1]):
+	line = line.strip()
+		L1.append(line)
+L2 = []
+for line in open(sys.argv[2]):
+        line = line.strip()
+                L1.append(line)
+eval = Eval(L1, L2)
 
-
+print eval.eval(L,L)
