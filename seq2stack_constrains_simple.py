8ca4564a0a10387d861030555264240896422cd8
Jiangming Liu <jiangming_liu@zhaoziyudeMacBook.local>
2017-12-18 21:17:30 +0000

    ^_^

--- seq2stack_constrains_simple.py
+++ seq2stack_constrains_simple.py
@@ -128,7 +128,7 @@ class AttnDecoderRNN(nn.Module):
                         stack_rep.pop()
                         stack_hidden.pop()
                         end -= 1
-                    stack_rep.append(tree_output)
+                    stack_rep.append(torch.sum(tree_output.view(2,1,-1),0))
 
                 output, hidden = self.lstm(stack_rep[-1], stack_hidden[-1])
                 stack_hidden.append(hidden)
