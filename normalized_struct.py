5edef657023da50cac7de7807b138074925450ed
Jiangming <jmliunlp@gmail.com>
2018-01-05 15:18:35 +0000

    ^_^

--- normalized_struct.py
+++ normalized_struct.py
@@ -5,10 +5,10 @@ for line in open(sys.argv[1]):
 	pnum = 1
 	knum = 1
 	for i in range(len(line)):
-		if line[i] == 'P1':
-			line[i] = 'P'+str(pnum)
+		if line[i] == 'P1(':
+			line[i] = 'P'+str(pnum)+'('
 			pnum += 1
-		elif line[i] == 'K1':
-			line[i] = 'K'+str(knum)
+		elif line[i] == 'K1(':
+			line[i] = 'K'+str(knum)+'('
 			knum += 1
 	print " ".join(line)
