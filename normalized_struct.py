104b9a955e71cce9a345890125171df7fa46a291
Jiangming Liu <jiangming_liu@zhaoziyudeMacBook.local>
2018-01-05 13:48:14 +0000

    ^_^

---
+++ normalized_struct.py
@@ -0,0 +1,14 @@
+import sys
+
+for line in open(sys.argv[1]):
+	line = line.strip().split()
+	pnum = 1
+	knum = 1
+	for i in range(len(line)):
+		if line[i] == 'P1':
+			line[i] = 'P'+str(pnum)
+			pnum += 1
+		elif line[i] == 'K1':
+			line[i] = 'K'+str(knum)
+			knum += 1
+	print " ".join(line)
