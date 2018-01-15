4941df1a894aa7ef2d60c670b387c90d3a075e87
Jiangming <jmliunlp@gmail.com>
2018-01-15 17:21:53 +0000

    ^_^

--- utils.py
+++ utils.py
@@ -253,6 +253,92 @@ def data2instance_structure_relation_variable(trn_data, ixes):
 			#print instances[-1][-1]
 	return instances
 
+def data2instance_structure_relation_variable2(trn_data, ixes):
+	instances = []
+	for one in trn_data:
+		instances.append([])
+		## words
+		instances[-1].append(torch.LongTensor([get_from_ix(w, ixes[0][0], ixes[0][1]) for w in one[0]]))
+		instances[-1].append(torch.LongTensor([get_from_ix(w, ixes[1][0], ixes[1][1]) for w in one[1]]))
+		instances[-1].append(torch.LongTensor([get_from_ix(w, ixes[2][0], ixes[2][1]) for w in one[2]]))
+
+		instances[-1].append([])
+		for item in one[3]:
+			type, idx = ixes[3].type(item)
+			if type == -2:
+				if idx == 4:
+					if relation == 0:
+						instances[-1][-1].append(idx)
+					elif relation > 0:
+						relation -= 1
+					else:
+						assert False
+				elif idx >= 5 and idx <= 12:
+					instances[-1][-1].append(idx)
+				elif idx >= ixes[3].k_rel_start and idx < ixes[3].p_rel_start:
+					instances[-1][-1].append(ixes[3].k_rel_start)
+				elif idx >= ixes[3].p_rel_start and idx < ixes[3].k_tag_start:
+					instances[-1][-1].append(ixes[3].p_rel_start)
+				elif idx >= 13 and idx < ixes[3].k_rel_start:
+					relation += 1
+			else:
+				relation += 1
+
+		instances[-1].append([])
+		stack = []
+		pointer = 0
+		#print "####"
+		for item in one[3]:
+			type, idx = ixes[3].type(item)
+			#print "==="
+			#print "stack:",stack
+			#print "type, idx", type, idx
+			#print "pointer",pointer
+			#print "item", item
+			if type == -2:
+				if idx == 4:
+					if stack[-1][0] >= 7 and stack[-1][0] <= 12:
+						pass
+					elif stack[-1][0] >= ixes[3].k_rel_start and stack[-1][0] < ixes[3].k_tag_start:
+						pass
+					elif stack[-1][0] == 5 or stack[-1][0] == 6:
+						pass
+					else:
+						instances[-1][-1][stack[-2][1]].append([stack[-1][1], stack[-1][0]])
+					stack.pop()
+				elif idx == 5 or idx == 6:
+					stack.append([idx, pointer])
+					instances[-1][-1].append([])
+					pointer += 1
+				elif idx >= 7 and idx < ixes[3].k_tag_start:
+					stack.append([idx, -2, -1])
+			else:
+				type = one[2].index(item[:-1])
+				idx = instances[-1][2].tolist()[type] + ixes[3].tag_size
+				assert type != -1 and idx != -1, "unrecogized local relation"
+				stack.append([idx, type, -1])
+
+		instances[-1].append([])
+		#print "####"
+		for item in one[3]:
+			type, idx = ixes[3].type(item)
+			#print "==="
+			#print "type, idx", type, idx
+			#print "item", item
+			if type == -2 and idx >= ixes[3].k_tag_start and idx < ixes[3].tag_size: #variable
+				if idx >= ixes[3].p_tag_start and idx < ixes[3].x_tag_start:
+					instances[-1][-1][-1].append(ixes[3].p_tag_start)
+				else:
+					instances[-1][-1][-1].append(idx)
+			elif type == -2 and (idx == 2 or idx == 3): # CARD_NUMBER and TIME_NUMBER
+				instances[-1][-1][-1].append(idx)
+			elif type == -1 or (type == -2 and idx >= 13 and idx < ixes[3].k_rel_start):
+				if len(instances[-1][-1]) != 0:
+					assert len(instances[-1][-1][-1]) <= 2 and len(instances[-1][-1][-1]) > 0
+				instances[-1][-1].append([])
+			#print instances[-1][-1]
+	return instances
+
 def data2instance_orig(trn_data, ixes):
 	instances = []
 	wix, wunk = ixes[0]
